ee698247aa0a7e403971d5769620ff8f336bca62
herculesnatan <medeiroshercules23@gmail.com>
2024-03-06 10:09:34 -0300

    Curso TypeScript: Zero to Hero

---
+++ modulo-02/aula-tipo-string/stringTypes.js
@@ -0,0 +1,11 @@
+"use strict";
+// Exemplo 01 - Single Quotes
+let nomeCompleto = 'Hércules Natan';
+console.log(nomeCompleto);
+// Exemplo 02 - Double Quotes 
+let funcaoEmpresa = 'FullStack Developer';
+console.log(funcaoEmpresa);
+// Exemplo 03 -Back Ticks
+let nomeEmpresa = "Microsoft";
+let dadosFuncionario = `Seja bem-vindo ${nomeCompleto}! Você será ${funcaoEmpresa} na empresa ${nomeEmpresa}.`;
+console.log(dadosFuncionario);
---
+++ modulo-02/aula-tipo-string/stringTypes.ts
@@ -0,0 +1,16 @@
+// Exemplo 01 - Single Quotes
+
+let nomeCompleto: string = 'Hércules Natan';
+console.log(nomeCompleto);
+
+// Exemplo 02 - Double Quotes 
+let funcaoEmpresa: string = 'FullStack Developer';
+console.log(funcaoEmpresa);
+
+// Exemplo 03 -Back Ticks
+
+let nomeEmpresa: string = "Microsoft";
+
+let dadosFuncionario:string = `Seja bem-vindo ${nomeCompleto}! Você será ${funcaoEmpresa} na empresa ${nomeEmpresa}.`;
+
+console.log(dadosFuncionario);
