34bb56eb555b26aaa0ca3c41298ca9990e4e0ee5
herculesnatan <medeiroshercules23@gmail.com>
2024-03-25 14:43:47 -0300

    Curso TypeScript: Zero to Hero

--- modulo-02/18-aula-tipo-tuple/tupleType.js
+++ modulo-02/18-aula-tipo-tuple/tupleType.js
@@ -7,10 +7,24 @@ console.log(pessoa);
 // Exemplo 02 - Acessando o valor da Tupla
 let pessoa1;
 pessoa1 = ['Hércules Natan', 'Computer Engineering Student', 21];
-console.log(pessoa1[1]);
+console.log(pessoa1[1]); // acessando valor da tupla
 // Exemplo 03 - Outra forma de usar Tuplas em TypeScript (com labels)
 let pessoa2 = ['Hércules Natan', 'Computer Engineering Student', 21];
 console.log(pessoa2);
 // Exemplo 04 - Usando Tuplas com Spread Operator
 let listaFrutas = ['🍍', '🍊', '🍎', '🍉', '🥭'];
 console.log(...listaFrutas);
+// Exemplo 05 - Lista Heterogênea de Tupla:
+let listaFrutas2 = [5, true, ...listaFrutas];
+console.log(listaFrutas2);
+// Exemplo 06 - Uso de função com Tuplas
+function listarPessoas(nomes, idades) {
+    return [...nomes, ...idades];
+}
+let resultado = listarPessoas(['Hercules', 'Maggie'], [21, 4]);
+console.log(resultado);
+function criarPessoa(...nome) {
+    return [...nome];
+}
+console.log(criarPessoa('Hércules', 'Medeiros'));
+console.log(criarPessoa('Hércules', 'Natan', 'Medeiros'));
--- modulo-02/18-aula-tipo-tuple/tupleType.ts
+++ modulo-02/18-aula-tipo-tuple/tupleType.ts
@@ -7,7 +7,7 @@ console.log(pessoa);
 // Exemplo 02 - Acessando o valor da Tupla
 let pessoa1: [string, string, number]; 
 pessoa1 = ['Hércules Natan', 'Computer Engineering Student', 21];
-console.log(pessoa1[1]);
+console.log(pessoa1[1]); // acessando valor da tupla
 
 // Exemplo 03 - Outra forma de usar Tuplas em TypeScript (com labels)
 let pessoa2: [nome: string, posicao: string, idade: number] = ['Hércules Natan', 'Computer Engineering Student', 21];
@@ -16,3 +16,28 @@ console.log(pessoa2);
 // Exemplo 04 - Usando Tuplas com Spread Operator
 let listaFrutas: [string, ...string[]] = ['🍍', '🍊', '🍎', '🍉', '🥭'];
 console.log(...listaFrutas);
+
+// Exemplo 05 - Lista Heterogênea de Tupla:
+let listaFrutas2: [number, boolean, ...string[]] = [5, true, ...listaFrutas];
+console.log(listaFrutas2);
+
+// Exemplo 06 - Uso de função com Tuplas
+function listarPessoas(nomes: string[], idades: number[]) {
+    return [...nomes, ...idades];
+}
+
+let resultado = listarPessoas(['Hercules','Maggie'], [21,4]);
+console.log(resultado);
+
+// Exemplo 07 - Labeled Tuples com Spread Operator numa função
+
+type Nome = 
+        | [primeiroNome: string, sobrenome: string]
+        | [primemiroNome: string, nomeMeio: string, sobrenome:string]
+
+function criarPessoa(...nome: Nome) {
+    return [...nome];
+}
+
+console.log(criarPessoa('Hércules','Medeiros'));
+console.log(criarPessoa('Hércules','Natan', 'Medeiros'));
